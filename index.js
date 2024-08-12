ff0c3b0d43fa559146a2a2922a7958d1b8fc733f
MRLiuZhuoqun <mr.liuzhuoqun@hotmail.com>
2024-08-12 19:08:20 +0800

    啊

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementsByTagName("body")[0].addEventListener("click", function () {
+document.getElementsByClassName("btn1")[0].addEventListener("click", function () {
   var sound = new Audio("./sounds/two-guns.mp3");
   sound.play();
 
