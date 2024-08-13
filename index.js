4cbc8ed25997385a92bdc3be92409f72ff1026bc
MRLiuZhuoqun <mr.liuzhuoqun@hotmail.com>
2024-08-13 09:19:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-document.getElementsByClassName("btn1")[0].addEventListener("click", function () {
-  var sound = new Audio("./sounds/two-guns.mp3");
-  sound.play();
-
-});
+document
+  .getElementsByClassName("btn1")[0]
+  .addEventListener("click", function () {
+    var sound = new Audio("./sounds/two-guns.mp3");
+    sound.play();
+  });
